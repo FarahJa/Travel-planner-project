445e71c326122efeca9de99df2f9e306673962c4
FarahJa <66690477+FarahJa@users.noreply.github.com>
2021-08-28 17:51:47 +0300

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { addTrip } from "./js/app.js"
+import { checkInput } from "./js/inputCheck.js"
+import {clearData} from "./js/app.js"
+import '@fortawesome/fontawesome-free/js/all'
+import './styles/main.scss'
+import './views/img/alternative.jpg'
+import './views/img/logoo.jpg'
+
+export { addTrip }
+export { checkInput }
+export {clearData}
+
